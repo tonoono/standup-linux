9e35d66440d8a45714d8c4da07156b9a6fcf0b2f
Antonio Arredondo <arredondo.antonio@gmail.com>
2020-03-29 22:21:22 -0600

    License changed to (Free)BSD license, small edits

--- archlinux/level_0_archlinux.sh
+++ archlinux/level_0_archlinux.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 ### -----
+### Level 0 Setup
+###
 ### This file is the root baremetal install for any Arch Linux server that I'm running
 ### -----
 
@@ -8,16 +10,16 @@
 echo "Level 0 >>> terminal/console applications"
 
 echo "Install zsh + extras apps ..."
-pacman -S --noconfirm zsh powerline zsh-autosuggestions zsh-completions zsh-lovers\
-  zsh-syntax-highlighting zsh-theme-powerlevel9k powerline awesome-terminal-fonts\
+pacman -S --noconfirm zsh zsh-autosuggestions zsh-completions zsh-lovers \
+  zsh-syntax-highlighting zsh-theme-powerlevel9k powerline awesome-terminal-fonts \
   powerline-fonts powerline-vim
 
 echo "Install terminal apps ..."
-pacman -S --noconfirm neovim vim neofetch tmux ranger atool elinks ffmpegthumbnailer\
+pacman -S --noconfirm neovim vim neofetch tmux ranger atool elinks ffmpegthumbnailer \
   highlight mediainfo odt2txt perl-image-exiftool poppler python-chardet sudo w3m
 
 echo "Install extra terminal apps ..."
-pacman -S --noconfirm python-virtualenv python-virtualenvwrapper\
+pacman -S --noconfirm python-virtualenv python-virtualenvwrapper curl wget \
   zsh-history-substring-search
 
 echo "Install version control apps ..."
