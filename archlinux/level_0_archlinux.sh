828718670a0e334f898ac95a87e7b9a83d9279de
Antonio Arredondo <arredondo.antonio@gmail.com>
2020-08-09 09:50:48 -0600

    Adding some command line options

--- archlinux/level_0_archlinux.sh
+++ archlinux/level_0_archlinux.sh
@@ -11,20 +11,42 @@ echo " "
 echo " >>>>> Level 0 >>> terminal/console applications"
 echo " "
 
-echo "Install zsh + extras apps ..."
-pacman -S --noconfirm zsh zsh-autosuggestions zsh-completions zsh-lovers \
-  zsh-syntax-highlighting zsh-theme-powerlevel9k powerline awesome-terminal-fonts \
-  powerline-fonts powerline-vim
+if [ "$#" -eq 0 ]; then
+  echo "Install zsh + extras apps ..."
+  pacman -S --noconfirm zsh zsh-autosuggestions zsh-completions zsh-lovers \
+    zsh-syntax-highlighting zsh-theme-powerlevel9k powerline awesome-terminal-fonts \
+    powerline-fonts powerline-vim
 
-echo "Install terminal apps ..."
-pacman -S --noconfirm neovim vim neofetch tmux ranger atool elinks ffmpegthumbnailer \
-  highlight mediainfo odt2txt perl-image-exiftool poppler python-chardet sudo w3m
+  echo "Install terminal apps ..."
+  pacman -S --noconfirm neovim vim neofetch tmux ranger atool elinks ffmpegthumbnailer \
+    highlight mediainfo odt2txt perl-image-exiftool poppler python-chardet sudo w3m
 
-echo "Install extra terminal apps ..."
-pacman -S --noconfirm python-virtualenv python-virtualenvwrapper curl wget \
-  zsh-history-substring-search
+  echo "Install extra terminal apps ..."
+  pacman -S --noconfirm python-virtualenv python-virtualenvwrapper curl wget \
+    zsh-history-substring-search
 
-echo "Install version control apps ..."
-pacman -S --noconfirm git tig
+  echo "Install version control apps ..."
+  pacman -S --noconfirm git tig
+else
+  if [ "$#" -eq 1 ]; then
+    if [ "$1" = "remove" ]; then
+      echo "Install zsh + extras apps ..."
+      pacman -S --noconfirm zsh zsh-autosuggestions zsh-completions zsh-lovers \
+        zsh-syntax-highlighting zsh-theme-powerlevel9k powerline awesome-terminal-fonts \
+        powerline-fonts powerline-vim
+
+      echo "Install terminal apps ..."
+      pacman -S --noconfirm neovim vim neofetch tmux ranger atool elinks ffmpegthumbnailer \
+        highlight mediainfo odt2txt perl-image-exiftool poppler python-chardet sudo w3m
+
+      echo "Install extra terminal apps ..."
+      pacman -S --noconfirm python-virtualenv python-virtualenvwrapper curl wget \
+        zsh-history-substring-search
+
+      echo "Install version control apps ..."
+      pacman -S --noconfirm git tig
+    fi
+  fi
+fi
 
 
--- archlinux/level_1_archlinux.sh
+++ archlinux/level_1_archlinux.sh
@@ -15,24 +15,31 @@ echo " "
 echo "Install X environment ..."
 pacman -S --noconfirm xorg-server xorg-xinit xclip xsel
 
-echo "Install i3 ..."
-pacman -S --noconfirm i3-gaps i3blocks i3lock i3status compton unclutter arandr
-
-echo "Install i3 support ..."
-pacman -S --noconfirm rxvt-unicode rxvt-unicode-terminfo urxvt-perls dmenu dunst\
-  network-manager-applet scrot ttf-hack ttf-linux-libertine ttf-inconsolata noto-fonts\
-  rofi feh python-netifaces python-psutil python-requests xorg-xdpyinfo
-
-echo "Install X apps ..."
-pacman -S --noconfirm qutebrowser mupdf xorg-xbacklight pulseaudio pavucontrol conky
-
 echo "Install development utils ..."
 pacman -S --noconfirm base-devel
 
 echo "Install terminal browser ..."
 pacman -S --noconfirm surfraw
 
-echo "Install network manager app ..."
-systemctl enable NetworkManager
+echo "Install X apps ..."
+pacman -S --noconfirm qutebrowser mupdf xorg-xbacklight pulseaudio pavucontrol conky
 
+
+if [ "$#" -eq 1 ]; then
+  if [ "$1" = "bare" ]; then
+    echo "Install i3 ..."
+    pacman -S --noconfirm i3-gaps i3blocks i3lock i3status compton unclutter arandr
+
+    echo "Install i3 support ..."
+    pacman -S --noconfirm rxvt-unicode rxvt-unicode-terminfo urxvt-perls dmenu dunst\
+      network-manager-applet scrot ttf-hack ttf-linux-libertine ttf-inconsolata noto-fonts\
+      rofi feh python-netifaces python-psutil python-requests xorg-xdpyinfo
+
+    echo "Install network manager app ..."
+    systemctl enable NetworkManager
+  elif [ "$1" = "min" ]; then
+    echo "Install LXQT ..."
+    pacman -S --noconfirm lxqt
+  fi
+fi
 
